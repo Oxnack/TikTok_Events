59eec2a2c61fd7c4313689580826350f1adf6207
Sergey5588 <93373949+Sergey5588@users.noreply.github.com>
2024-11-16 17:01:13 +0300

    rename in Program.cs

--- TikTok_Events_dotnet/Program.cs
+++ TikTok_Events_dotnet/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using TikTok_Events;
+using SystemEffects;
 public class WebSocketClient
 {
     private ClientWebSocket _websocket = null;
